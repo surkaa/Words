eb8df34aa2ee2842e568fe9cdd4da1db9e4a1594
winingYang <suralcarywn@qq.com>
2022-10-30 11:20:04 +0800

    增加设置:默认关闭卡片

--- app/src/main/java/com/surkaa/wordsfortjnu/HelpActivity.java
+++ app/src/main/java/com/surkaa/wordsfortjnu/HelpActivity.java
@@ -22,7 +22,7 @@ public class HelpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_help);
+        setContentView(R.layout.activity_settings);
 
         repository = new WordRepository(getApplication());
 
